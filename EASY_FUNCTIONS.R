d7516173e6f2d28313f1a9dbdb49032f3e62297a
haachicanoy <harold22010@gmail.com>
2015-12-09 22:28:41 -0500

    Changes to banana

--- EASY_FUNCTIONS.R
+++ EASY_FUNCTIONS.R
@@ -1,16 +1,17 @@
 #############################################################################################
-#This script contains methodologies for quality control for hourly and daily climatic series
+# This script contains methodologies for quality control for hourly and daily climatic series
 #
-#This script is free: you can redistribute it and/or modify
-#Autor: VICTOR HUGO PATIÑO BRAVO
-#v.h.patino@cgiar.org
-#Agosto, 2015
-#Version V.02.15
-#This program is distributed in the hope that it will be useful,
-#but WITHOUT ANY WARRANTY; without even the implied warranty of
-#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+# This script is free: you can redistribute it and/or modify
+# Autor: VICTOR HUGO PATIÑO BRAVO
+# v.h.patino@cgiar.org
+# Agosto, 2015
+# Version V.02.15
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+#############################################################################################
 
-#PACKAGES
+# PACKAGES
 if(require(ggplot2)==FALSE){install.packages("ggplot2")} 
 if(require(rpart)==FALSE){ install.packages("rpart")}
 if(require(RMAWGEN)==FALSE){install.packages("RMAWGEN") } 
@@ -23,17 +24,17 @@ if(require(sirad)==FALSE){install.packages("sirad")}
 if(require(tidyr)==FALSE){install.packages("tidyr")}
 if(require(dplyr)==FALSE){install.packages("dplyr")}
 
-
-#Work directory  :: #dirFol    <- "C:/Users/nameUser/Desktop/workspace/"
-dirFol    <- "//dapadfs/workspace_cluster_6/TRANSVERSAL_PROJECTS/MADR/COMPONENTE_2/CLIMA/SERIES_CLIMA_PROCESADO/Cundinamarca/CundinamarcaBoyaca/"
+# Work directory  :: #dirFol    <- "C:/Users/nameUser/Desktop/workspace/"
+dirFol    <- "//dapadfs/workspace_cluster_6/TRANSVERSAL_PROJECTS/MADR/COMPONENTE_2/ASBAMA/DATOS_PROCESADOS/_clima/IDEAM"
 setwd(dirFol)
 
 GRAPHICS  <- function(){source(paste0(dirFol,"/GRAPHICS.R"))}
-#Years of analisys
 
-#Para Tolima
-YStart    <-  2011#Star Year for analisys
-YEnd      <-  2014#End  Year for analisys
+# Years of analisys
+
+# Para Magdalena
+YStart    <-  2008 # Star Year for analisys
+YEnd      <-  2015 # End  Year for analisys
 
                                   ########  ########  ######   #### ##    ## 
                                   ##     ## ##       ##    ##   ##  ###   ## 
@@ -52,6 +53,7 @@ YEnd      <-  2014#End  Year for analisys
 
 #Create folders
 FOLDERS(dirFol)
+
         ########################################################################
         #WARNING!!!! :You need put files on SERIES_ORIGINAL folder for continue#
         ########################################################################
